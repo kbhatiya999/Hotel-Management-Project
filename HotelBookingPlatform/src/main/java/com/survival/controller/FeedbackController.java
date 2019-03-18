dddb429e8e78d219f5a4363c2d05302f4c801aaf
arnav agyeya <arnav.agyeya@gmail.com>
2019-03-18 15:02:53 +0530

    session

--- HotelBookingPlatform/src/main/java/com/survival/controller/FeedbackController.java
+++ HotelBookingPlatform/src/main/java/com/survival/controller/FeedbackController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.survival.entities.Feedback;
@@ -13,7 +14,7 @@ import com.survival.persistence.FeedbackDao;
 import com.survival.service.FeedbackService;
 
 
-
+@SessionAttributes("hid")
 @Controller
 public class FeedbackController {
 	
